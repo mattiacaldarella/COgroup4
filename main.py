71c4ab077763fbf8fd67359bddc0fd3899d52c16
mocaldarella <122272713+mocaldarella@users.noreply.github.com>
2023-04-12 11:51:38 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+def ReadInstance():
+	print("reading instance... \U0001f44d")
+
+
+
+def Optimize():
+	print("optimizing... \U0001f95d")
+
+
+
+def WriteResults():
+	print("writing results to file... \U0001f48d")
+
+
+
+if __name__ == "__main__":
+	ReadInstance()
+	Optimize()
+	WriteResults()
