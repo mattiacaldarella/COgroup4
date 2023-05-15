e1efb87b27ab575183af0288061f9eedd1b75225
pietervierdag <pieter@vierdag.nl>
2023-05-15 13:13:28 +0200

    Cleaner code

--- Optimize2.py
+++ Optimize2.py
@@ -13,14 +13,12 @@ def optimize2(problem_data: ProblemData):
     solution = Solution()
 
     dic_request, dic_pickup = day_divider1(problem_data)
-    theta = polar_order(problem_data) #, r
+    theta = polar_order(problem_data)
+
     dist_matrix = distance_matrix1(problem_data)
     available_tools = {tool.id: tool.number_available for tool in problem_data.tools}
-    #print(available_tools)
-    newlist = list(dic_request.keys()) + list(dic_pickup.keys())
-    unique_lst = list(set(newlist))
 
-    for j in sorted(unique_lst):
+    for j in sorted(list(set(dic_request) | set(dic_pickup))):
         available_count_re = {}
         available_count_pi = {}
         tot_routes = []
@@ -34,12 +32,19 @@ def optimize2(problem_data: ProblemData):
 
         solution.routes[j] = tot_routes
 
-        available_tools = {key: value + available_count_re.get(key, 0) + available_count_pi.get(key, 0) for key, value in available_tools.items()}
-        #print(j)
-        #print(available_count_re)
-        #print(available_count_pi)
-        #print(available_tools)
+        ###
+        available_tools_enough = {key: value + available_count_re.get(key, 0) for key, value in available_tools.items()}
 
+        found_negative = False
+        for key, value in available_tools_enough.items():
+            if -value in available_tools_enough.values():
+                found_negative = True
+                break
+
+        if found_negative:
+            print("yes")
+        available_tools = {key: value + available_count_pi.get(key, 0) for key, value in available_tools.items()}
+        ###
 
     return solution
 
@@ -48,45 +53,33 @@ def sweep_method(theta: list, lst: list, problem_data: ProblemData, dist_matrix:
     routes = []
     i = 0
     tool_sizes = {tool.id: tool.size for tool in problem_data.tools}
+    available_tools = {tool.id: 0 for tool in problem_data.tools}
 
     while i < len(sort):
-        i, route, available_tools = request_vs_pickup(i, sort, tool_sizes, problem_data, sgn)
-        test, rout = gurobi(route, problem_data, dist_matrix)
+        i, route = request_vs_pickup(i, sort, tool_sizes, problem_data, sgn) #, available_tools
+        test, routx = gurobi(route, problem_data, dist_matrix)
 
-        #exit()
         while test == False:
-            if sgn == True:
-                available_tools[sort[i-1].tool_kind_id] += 1 #check this
-            else:
-                available_tools[sort[i-1].tool_kind_id] -= 1
-
             del route[-1]
-            test, rout = gurobi(route, problem_data, dist_matrix)
-
             i -= 1
+            test, routx = gurobi(route, problem_data, dist_matrix)
 
-        if sgn == False:
-            for j in range(len(rout)):
-                rout[j] *= -1
+        route_ids = [0] + [r.id for r in routx] + [0]
+        if not sgn:
+            route_ids = [-i for i in route_ids]
+        for r in routx:
+            available_tools[r.tool_kind_id] += (-1) ** sgn * r.tools_needed
+        routes.append(route_ids)
 
-        routes.append(rout)
     return routes, available_tools
 
 def request_vs_pickup(i: int, sort: dict, tool_sizes: dict, problem_data: ProblemData, sgn: bool):
     fill = tool_sizes.get(sort[i].tool_kind_id) * sort[i].tools_needed
-    available_tools = {tool.id: 0 for tool in problem_data.tools}
-    cap = []
     route = []
 
     while problem_data.capacity >= fill and i < len(sort):
-        cap.append(fill)
         route.append(sort[i])
         fill += tool_sizes.get(sort[i].tool_kind_id) * sort[i].tools_needed
-
-        if sgn == True:
-            available_tools[sort[i].tool_kind_id] -= 1
-        else:
-            available_tools[sort[i].tool_kind_id] += 1
 
         i += 1
         if i >= len(sort):
@@ -94,10 +87,10 @@ def request_vs_pickup(i: int, sort: dict, tool_sizes: dict, problem_data: Proble
 
         fill += tool_sizes.get(sort[i].tool_kind_id) * sort[i].tools_needed
 
-    return i, route, available_tools
+    return i, route
 
 def gurobi(route: list, problem_data: ProblemData, dist_matrix: np.ndarray):
-    route_id_list, dup_dic = give_duplicates(route)
+    route_id_list, dup_dic = give_duplicates(route) #check
 
     m = gp.Model('TSP')
 
@@ -131,57 +124,40 @@ def gurobi(route: list, problem_data: ProblemData, dist_matrix: np.ndarray):
 
     m.optimize()
 
-    maxdis = 0
-
     # Print the tour_order if an optimal solution is found
     if m.status == GRB.OPTIMAL:
-        #print(f"Optimal objective value: {m.objVal:.2f}")
         tour_order = [0]
         i = 0
         while len(tour_order) < n:
             for j in range(n):
                 if x[i, j].x > 0.5 and j not in tour_order:
                     tour_order.append(j)
-                    maxdis += x[i, j].x * reduced_dist_matrix[i][j]
                     i = j
                     break
+        tour_order.remove(0)
 
         finish = compiler(tour_order, route)
 
-        if maxdis > problem_data.max_trip_distance:
-            print('no', maxdis)
         return True, finish
     else:
         return False, None
 
 def compiler(tour_order: list, route: list):
-    tour_order.remove(0)
-
-    dub = {}
+    dub = {i.location_id: [] for i in route}
     for i in route:
-        if i.location_id in dub.keys():
-            dub[i.location_id].append(i)
-        else:
-            dub[i.location_id] = [i]
+        dub[i.location_id].append(i)
 
-    n = len(dub.keys())
-    new_dub = {}
-    for i, key in enumerate(dub.keys()):
-        new_dub[i] = dub[key]
-
-    defaultlist = [0] * n
-
-    for i in tour_order:
-        defaultlist[i - 1] = new_dub.get(i - 1)
+    new_dub = {i: dub[key] for i, key in enumerate(dub)}
+    defaultlist = [new_dub.get(i - 1, 0) for i in tour_order]
 
     flat_list = [x for sublist in defaultlist for x in sublist]
-    plst = []
+    finish = []
 
     for x in flat_list:
-        plst.append(x.id)
+        finish.append(x)
 
-    finish = [0] + plst + [0]
     return finish
+
 
 def give_duplicates(lst: list):
     new = []
